c3d52c2125ae423534e641526fc24a010056bcd7
Sebastian Monzel <dem_github_com@sebastianmonzel.de>
2015-06-22 01:25:19 +0200

    documentation

--- source/core/datastore/MAbstractDatastore.php
+++ source/core/datastore/MAbstractDatastore.php
@@ -30,44 +30,72 @@ use simpleserv\webfilesframework\core\datasystem\file\system\MDirectory;
 abstract class MAbstractDatastore extends MWebfile {
 	
 	/**
-	 * checks if a connection is possible
+	 * Checks if a connection is possible.
 	 */
 	public abstract function tryConnect();
 	
 	/**
-	 * determines if the datastore is read-only or not.
-	 * @return boolean
+	 * Determines if the datastore is read-only or not.
+	 * @return boolean information if datastore is readonly or not.
 	 */
 	public abstract function isReadOnly();
 	
 	/**
 	 * Returns the next webfile for the given timestamp
-	 * @param $time timestamp (unix-format)
+	 * @param int $time timestamp in unix-format.
+	 * @return MWebfile webfile according to the given input.
 	 */
 	public abstract function getNextWebfileForTime($time);
 	
 	/**
-	 * Returns a webfiles stream from the actual datastore.
+	 * Returns a webfiles stream with all webfiles from 
+	 * the actual datastore.
 	 */
 	public abstract function getWebfilestream();
 	
 	/**
-	 * 
+	 * Returns all webfiles from the actual datastore.
+	 * @return array list of webfiles
+	 */
+	public abstract function getWebfilesFromDatastore();
+	
+	/**
+	 * Returns all webfile datasets from the actual datastore.
+	 * @return array list of datasets
 	 */
 	public abstract function getDatasetsFromDatastore();
 	
+	
+	
+	/**
+	 * Returns the latests webfiles. Sorting will
+	 * happen according to the time information of the webfiles.
+	 *
+	 * @param int $count Count of webfiles to be selected.
+	 * @return list of webfiles
+	 */
+	public abstract function getLatestWebfiles($count = 5);
+	
+	/**
+	 * Returns the latests webfile datasets. Sorting will 
+	 * happen according to the time information of the webfiles.
+	 *  
+	 * @param int $count Count of webfiles to be selected.
+	 * @param string $reverse Sorting of the given webfiles (Newest 
+	 * first or oldest firs).
+	 * @return list of datasets
+	 */
 	public abstract function getLatestDatasets($count = 5, $reverse = true);
 	
-	public abstract function getWebfilesFromDatastore();
 	
-	public abstract function getLatestWebfiles($count = 5);
+	
 	
 	/**
 	 * Returns a set of webfiles in the actual datastore which matches
 	 * with the given template.<br />
 	 * Searching by template is devided in two steps:<br />
 	 * <ol>
-	 * 	<li>On the first step you define the template you want to search with</li>
+	 * 	<li>On the first step you define the template you want to search with. Here can help you the method <b>presetDefaultForTemplate</b> on the class <b>MWebfile</b>.</li>
 	 *  <li>On the second step you put the template to the datastore to start the search</li>
 	 * </ol>
 	 * 
@@ -92,6 +120,7 @@ abstract class MAbstractDatastore extends MWebfile {
 	
 	/**
 	 * Stores a single webfile in the datastore.
+	 * 
 	 * @param MWebfile $webfile
 	 * @throws MDatastoreException
 	 */
@@ -120,7 +149,10 @@ abstract class MAbstractDatastore extends MWebfile {
 	
 	/**
 	 * Resolves a datastore which is localized in the folder
-	 * <b>"./custom/datastore"</b> according to the given id.
+	 * <b>"./custom/datastore"</b> according to the given id.<br />
+	 * Every file situated in the datastore folder will be converted
+	 * to a webfile an the list of webfiles will be used to compare
+	 * the id on each datastore in the folder. 
 	 * 
 	 * @param string $datastoreId
 	 * @throws MDatastoreException will be thrown if no datastore with
@@ -147,9 +179,7 @@ abstract class MAbstractDatastore extends MWebfile {
 			throw new MDatastoreException("Cannot find datastore for id '". $datastoreId . "'");
 		}
 		
-		
 		return $selectedWebfile;
-		
 	}
 	
 }
