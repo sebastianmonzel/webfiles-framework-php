8ccb049543eb13f0d3e5d1695d709b69f3bea1ce
Sebastian Monzel <dem_github_com@sebastianmonzel.de>
2016-12-30 11:22:53 +0100

    differ xml validation exceptions more specific

--- source/core/datastore/webfilestream/MWebfileStream.php
+++ source/core/datastore/webfilestream/MWebfileStream.php
@@ -64,38 +64,53 @@ class MWebfileStream
 
         $webfilesResultArray = array();
 
-        $root = @simplexml_load_string($input);
+        $root = $this->parseAndValidateWebfilesStreamXml($input);
+        $webfilesChildren = $root->webfiles->children();
 
-        if ($root != null && $root != false) {
-
-            $rootChildren = $root->children();
-
-            if ( count($rootChildren) != 1 ) {
-                throw new MWebfilesFrameworkException("Root element has not exactly one child. Input: " . $input);
-            }
-
-            /** @var \SimpleXMLElement $rootChild */
-            foreach ($rootChildren as $rootChild) {
-                if ( $rootChild->getName() != "webfiles" ) {
-                    throw new MWebfilesFrameworkException("No webfiles child exists on root element. Input: " . $input);
-                }
-            }
-
-            $webfilesChildren = $root->webfiles->children();
-
-            /** @var \SimpleXMLElement $webfileChild */
-            foreach ($webfilesChildren as $webfileChild) {
-                array_push(
-                    $webfilesResultArray, MWebfile::staticUnmarshall($webfileChild->asXML()));
-            }
-
-        } else {
-            throw new MWebfilesFrameworkException(
-                "Error on reading xml of webfile stream: No root element given. Input: " . $input);
+        /** @var \SimpleXMLElement $webfileChild */
+        foreach ($webfilesChildren as $webfileChild) {
+            array_push(
+                $webfilesResultArray, MWebfile::staticUnmarshall($webfileChild->asXML()));
         }
 
         return $webfilesResultArray;
     }
+
+    /**
+     * @param string $input
+     * @return \SimpleXMLElement
+     * @throws MWebfilesFrameworkException
+     */
+    private function parseAndValidateWebfilesStreamXml($input) {
+
+        $root = @simplexml_load_string($input);
+
+        if ( $root == null ) {
+            throw new MWebfilesFrameworkException(
+                "Error on reading xml of webfile stream: No root element given. Input: " . $input);
+        }
+
+        if ( $root === false ) {
+            throw new MWebfilesFrameworkException(
+                "Error on reading xml of webfile stream: Input: " . $input);
+        }
+
+        $rootChildren = $root->children();
+
+        if ( count($rootChildren) != 1 ) {
+            throw new MWebfilesFrameworkException("Root element has not exactly one child. Input: " . $input);
+        }
+
+        /** @var \SimpleXMLElement $rootChild */
+        foreach ($rootChildren as $rootChild) {
+            if ( $rootChild->getName() != "webfiles" ) {
+                throw new MWebfilesFrameworkException("No webfiles child exists on root element. Input: " . $input);
+            }
+        }
+
+        return $root;
+    }
+
 
     public function getXML()
     {
