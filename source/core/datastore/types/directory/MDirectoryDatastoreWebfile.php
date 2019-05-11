64a4caf85d9fda2eb642b35f3cfa1fbc5b9cfb94
Sebastian Monzel <mail@sebastianmonzel.de>
2019-05-11 09:17:15 +0200

    adjusted docu

--- source/core/datastore/types/directory/MDirectoryDatastoreWebfile.php
+++ source/core/datastore/types/directory/MDirectoryDatastoreWebfile.php
@@ -33,90 +33,90 @@ class MDirectoryDatastoreWebfile extends MAbstractDatastore
     public static $m__sClassName = __CLASS__;
 
 
-    /* (non-PHPdoc)
-     * @see \webfilesframework\core\datastore\MAbstractDatastore::tryConnect()
-     */
+	/**
+	 * @see \webfilesframework\core\datastore\MAbstractDatastore::tryConnect()
+	 */
     public function tryConnect()
     {
         $this->initDatastore();
         return $this->directoryDatastore->tryConnect();
     }
 
-    /* (non-PHPdoc)
-     * @see \webfilesframework\core\datastore\MAbstractDatastore::isReadOnly()
-     */
+	/**
+	 * @see \webfilesframework\core\datastore\MAbstractDatastore::isReadOnly()
+	 */
     public function isReadOnly()
     {
         $this->initDatastore();
         return $this->directoryDatastore->isReadOnly();
     }
 
-    /* (non-PHPdoc)
-     * @see \webfilesframework\core\datastore\MAbstractDatastore::getNextWebfileForTimestamp()
-     */
+	/**
+	 * @see \webfilesframework\core\datastore\MAbstractDatastore::getNextWebfileForTimestamp()
+	 */
     public function getNextWebfileForTimestamp($time)
     {
         $this->initDatastore();
         return $this->directoryDatastore->getNextWebfileForTimestamp($time);
     }
 
-    /* (non-PHPdoc)
-     * @see \webfilesframework\core\datastore\MAbstractDatastore::getWebfilesAsStream()
-     */
+	/**
+	 * @see \webfilesframework\core\datastore\MAbstractDatastore::getWebfilesAsStream()
+	 */
     public function getWebfilesAsStream()
     {
         $this->initDatastore();
         return $this->directoryDatastore->getWebfilesAsStream();
     }
 
-    /* (non-PHPdoc)
-     * @see \webfilesframework\core\datastore\MAbstractDatastore::getWebfilesAsArray()
-     */
+	/**
+	 * @see \webfilesframework\core\datastore\MAbstractDatastore::getWebfilesAsArray()
+	 */
     public function getWebfilesAsArray()
     {
         $this->initDatastore();
         return $this->directoryDatastore->getWebfilesAsArray();
     }
 
-    /* (non-PHPdoc)
-     * @see \webfilesframework\core\datastore\MAbstractDatastore::getLatestWebfiles()
-     */
+	/**
+	 * @see \webfilesframework\core\datastore\MAbstractDatastore::getLatestWebfiles()
+	 */
     public function getLatestWebfiles($count = 5)
     {
         $this->initDatastore();
         return $this->directoryDatastore->getLatestWebfiles($count);
     }
 
-    /* (non-PHPdoc)
-     * @see \webfilesframework\core\datastore\MAbstractDatastore::getByTemplate()
-     */
+	/**
+	 * @see \webfilesframework\core\datastore\MAbstractDatastore::getByTemplate()
+	 */
     public function searchByTemplate(MWebfile $template)
     {
         $this->initDatastore();
         return $this->directoryDatastore->searchByTemplate($template);
     }
 
-    /* (non-PHPdoc)
-     * @see \webfilesframework\core\datastore\MAbstractDatastore::storeWebfile()
-     */
+	/**
+	 * @see \webfilesframework\core\datastore\MAbstractDatastore::getByTemplate()
+	 */
     public function storeWebfile(MWebfile $webfile)
     {
         $this->initDatastore();
         return $this->directoryDatastore->storeWebfile($webfile);
     }
 
-    /* (non-PHPdoc)
-     * @see \webfilesframework\core\datastore\MAbstractDatastore::storeWebfilesFromStream()
-     */
+	/**
+	 * @see \webfilesframework\core\datastore\MAbstractDatastore::storeWebfilesFromStream()
+	 */
     public function storeWebfilesFromStream(MWebfileStream $webfileStream)
     {
         $this->initDatastore();
         return $this->directoryDatastore->storeWebfilesFromStream($webfileStream);
     }
 
-    /* (non-PHPdoc)
-     * @see \webfilesframework\core\datastore\MAbstractDatastore::deleteByTemplate()
-     */
+	/**
+	 * @see \webfilesframework\core\datastore\MAbstractDatastore::deleteByTemplate()
+	 */
     public function deleteByTemplate(MWebfile $template)
     {
         $this->initDatastore();
