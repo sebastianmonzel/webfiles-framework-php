9e71a3288118a53d9066686069762eb1f9bcff57
sebastianmonzel <mail@sebastianmonzel.de>
2026-02-17 21:39:33 +0100

    split remote datastore test into xml and json

---
+++ tests/source/core/datastore/types/remote/MRemoteDatastoreJsonTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace test\webfilesframework\core\datastore\types\remote;
+
+use ReflectionException;
+use test\webfilesframework\MAbstractWebfilesFramworkTest;
+use webfilesframework\core\datastore\types\database\MSampleWebfile;
+use webfilesframework\core\datastore\types\remote\MRemoteDatastore;
+use webfilesframework\MWebfilesFrameworkException;
+
+class MRemoteDatastoreJsonTest extends MAbstractWebfilesFramworkTest {
+
+	protected $object;
+
+    public function createJsonRemoteDatastore(): MRemoteDatastore {
+        return new MRemoteDatastore(
+            "http://webfiles.sebastianmonzel.de/jenkins/datastore/","json"
+        );
+    }
+
+    /**
+     * @throws MWebfilesFrameworkException
+     * @throws ReflectionException
+     */
+    public function test_json_getAllWebfiles() {
+
+        $jsonRemoteDatastore = $this->createJsonRemoteDatastore();
+
+        $this->doTestGetAllWebfiles($jsonRemoteDatastore);
+    }
+
+    /**
+     * @throws MWebfilesFrameworkException
+     * @throws ReflectionException
+     */
+    public function test_json_storeWebfileAndDeleteItAgain() {
+
+        $jsonRemoteDatastore = $this->createJsonRemoteDatastore();
+        $this->doTestStoreAndDelete($jsonRemoteDatastore);
+    }
+
+    /**
+     * @param MRemoteDatastore $remoteDatastore
+     * @throws MWebfilesFrameworkException
+     * @throws ReflectionException
+     */
+    private function doTestStoreAndDelete(MRemoteDatastore $remoteDatastore): void
+    {
+        $webfileToStore = new MSampleWebfile();
+		$webfileToStore->setLastname("Schmidt");
+
+        $webfileToStore->setId(4);
+        $webfilesStream = $remoteDatastore->storeWebfile($webfileToStore);
+        self::assertCount(3, $webfilesStream->getArray());
+
+        $webfileToStore->setId(5);
+        $webfilesStream = $remoteDatastore->storeWebfile($webfileToStore);
+        self::assertCount(4, $webfilesStream->getArray());
+
+        $searchtemplate = new MSampleWebfile();
+        $searchtemplate->presetForTemplateSearch();
+		$searchtemplate->setLastname("Schmidt");
+        $webfilesStream = $remoteDatastore->deleteByTemplate($searchtemplate);
+
+        self::assertGreaterThanOrEqual(2, $webfilesStream->getArray());
+    }
+
+    /**
+     * @param MRemoteDatastore $remoteDatastore
+     * @throws MWebfilesFrameworkException
+     * @throws ReflectionException
+     */
+    private function doTestGetAllWebfiles(MRemoteDatastore $remoteDatastore): void
+    {
+        $webfilesAsStream = $remoteDatastore->getAllWebfiles();
+
+        self::assertNotNull($webfilesAsStream);
+        $webfilesArray = $webfilesAsStream->getArray();
+        self::assertTrue(is_array($webfilesArray));
+        self::assertCount(2, $webfilesArray);
+
+        /** @var MSampleWebfile $firstWebfile */
+        $firstWebfile = $webfilesArray[0];
+
+        self::assertEquals("Sebastian", $firstWebfile->getFirstname());
+        self::assertEquals("Monzel", $firstWebfile->getLastname());
+    }
+}
--- tests/source/core/datastore/types/remote/MRemoteDatastoreXmlTest.php
+++ tests/source/core/datastore/types/remote/MRemoteDatastoreXmlTest.php
@@ -1,16 +1,14 @@
 <?php
 
-
-
-namespace test\webfilesframework\core\datastore\types;
+namespace test\webfilesframework\core\datastore\types\remote;
 
 use ReflectionException;
 use test\webfilesframework\MAbstractWebfilesFramworkTest;
 use webfilesframework\core\datastore\types\database\MSampleWebfile;
 use webfilesframework\core\datastore\types\remote\MRemoteDatastore;
 use webfilesframework\MWebfilesFrameworkException;
-/*
-class MRemoteDatastoreTest extends MAbstractWebfilesFramworkTest {
+
+class MRemoteDatastoreXmlTest extends MAbstractWebfilesFramworkTest {
 
 	protected $object;
 
@@ -19,12 +17,6 @@ class MRemoteDatastoreTest extends MAbstractWebfilesFramworkTest {
 			"http://webfiles.sebastianmonzel.de/jenkins/datastore/","xml"
 		);
 	}
-
-    public function createJsonRemoteDatastore() {
-        return new MRemoteDatastore(
-            "http://webfiles.sebastianmonzel.de/jenkins/datastore/","json"
-        );
-    }
 
 	/**
 	 * @throws MWebfilesFrameworkException
@@ -36,17 +28,6 @@ class MRemoteDatastoreTest extends MAbstractWebfilesFramworkTest {
 
         $this->doTestGetAllWebfiles($xmlRemoteDatastore);
 	}
-
-    /**
-     * @throws MWebfilesFrameworkException
-     * @throws ReflectionException
-     */
-    public function test_json_getAllWebfiles() {
-
-        $jsonRemoteDatastore = $this->createJsonRemoteDatastore();
-
-        $this->doTestGetAllWebfiles($jsonRemoteDatastore);
-    }
 
 	/**
 	 * @throws MWebfilesFrameworkException
@@ -103,69 +84,6 @@ class MRemoteDatastoreTest extends MAbstractWebfilesFramworkTest {
 		$this->doTestStoreAndDelete($xmlRemoteDatastore);
 	}
 
-    /**
-     * @throws MWebfilesFrameworkException
-     * @throws ReflectionException
-     */
-    public function test_json_storeWebfileAndDeleteItAgain() {
-
-        $jsonRemoteDatastore = $this->createJsonRemoteDatastore();
-        $this->doTestStoreAndDelete($jsonRemoteDatastore);
-    }
-
-    /**
-     * @param MRemoteDatastore $remoteDatastore
-     * @throws MWebfilesFrameworkException
-     * @throws ReflectionException
-     */
-    private function doTestStoreAndDelete(MRemoteDatastore $remoteDatastore): void
-    {
-        $webfileToStore = new MSampleWebfile();
-		$webfileToStore->setLastname("Schmidt");
-        /*$webfileToStore->setLastname("Schmidt
-        // TODO zeil4enumbruch macht probleme bei json
-        ");*/
-
-        $webfileToStore->setId(4);
-        $webfilesStream = $remoteDatastore->storeWebfile($webfileToStore);
-        self::assertCount(3, $webfilesStream->getArray());
-
-        $webfileToStore->setId(5);
-        $webfilesStream = $remoteDatastore->storeWebfile($webfileToStore);
-        self::assertCount(4, $webfilesStream->getArray());
-
-        $searchtemplate = new MSampleWebfile();
-        $searchtemplate->presetForTemplateSearch();
-		$searchtemplate->setLastname("Schmidt");
-        /*$searchtemplate->setLastname("Schmidt
-        // TODO zeil4enumbruch macht probleme bei json
-        ");*/
-        $webfilesStream = $remoteDatastore->deleteByTemplate($searchtemplate);
-
-		$searchtemplate = new MSampleWebfile();
-        $searchtemplate->presetForTemplateSearch();
-		$searchtemplate->setLastname("Schmidt\\n        // TODO zeil4enumbruch macht probleme bei json\\n        ");
-        /*$searchtemplate->setLastname("Schmidt
-        // TODO zeil4enumbruch macht probleme bei json
-        ");*/
-		$webfilesStream = $remoteDatastore->deleteByTemplate($searchtemplate);
-
-
-		
-        self::assertGreaterThanOrEqual(2, $webfilesStream->getArray());
-    }
-    /*
-	public function testGetNextWebfileForTimestamp() {
-
-		$remoteDatastore = $this->createRemoteDatastore();
-
-		$next_webfile_for_timestamp = $remoteDatastore->getNextWebfileForTimestamp( -1 );
-		// TODO für die methode  muss man normalize auf dem directory datastore aufrufen -
-		// TODO normalize gibt es jedoch nicht im remote datastore - soll ich die normalize durchleiten?
-		//self::assertNotNull($next_webfile_for_timestamp);
-		self::assertEquals("","");
-	}*/
-
 	public function test_xml_isReadonly() {
 
 		$remoteDatastore = $this->createXmlRemoteDatastore();
@@ -194,13 +112,44 @@ class MRemoteDatastoreTest extends MAbstractWebfilesFramworkTest {
 	}
 
     /**
-     * @param MRemoteDatastore $jsonRemoteDatastore
+     * @param MRemoteDatastore $remoteDatastore
      * @throws MWebfilesFrameworkException
      * @throws ReflectionException
      */
-    private function doTestGetAllWebfiles(MRemoteDatastore $jsonRemoteDatastore): void
+    private function doTestStoreAndDelete(MRemoteDatastore $remoteDatastore): void
     {
-        $webfilesAsStream = $jsonRemoteDatastore->getAllWebfiles();
+        $webfileToStore = new MSampleWebfile();
+		$webfileToStore->setLastname("Schmidt");
+
+        $webfileToStore->setId(4);
+        $webfilesStream = $remoteDatastore->storeWebfile($webfileToStore);
+        self::assertCount(3, $webfilesStream->getArray());
+
+        $webfileToStore->setId(5);
+        $webfilesStream = $remoteDatastore->storeWebfile($webfileToStore);
+        self::assertCount(4, $webfilesStream->getArray());
+
+        $searchtemplate = new MSampleWebfile();
+        $searchtemplate->presetForTemplateSearch();
+		$searchtemplate->setLastname("Schmidt");
+        $webfilesStream = $remoteDatastore->deleteByTemplate($searchtemplate);
+
+		$searchtemplate = new MSampleWebfile();
+        $searchtemplate->presetForTemplateSearch();
+		$searchtemplate->setLastname("Schmidt\\n        // TODO zeil4enumbruch macht probleme bei json\\n        ");
+		$webfilesStream = $remoteDatastore->deleteByTemplate($searchtemplate);
+
+        self::assertGreaterThanOrEqual(2, $webfilesStream->getArray());
+    }
+
+    /**
+     * @param MRemoteDatastore $remoteDatastore
+     * @throws MWebfilesFrameworkException
+     * @throws ReflectionException
+     */
+    private function doTestGetAllWebfiles(MRemoteDatastore $remoteDatastore): void
+    {
+        $webfilesAsStream = $remoteDatastore->getAllWebfiles();
 
         self::assertNotNull($webfilesAsStream);
         $webfilesArray = $webfilesAsStream->getArray();
@@ -213,6 +162,4 @@ class MRemoteDatastoreTest extends MAbstractWebfilesFramworkTest {
         self::assertEquals("Sebastian", $firstWebfile->getFirstname());
         self::assertEquals("Monzel", $firstWebfile->getLastname());
     }
-
-
-}*/
+}
